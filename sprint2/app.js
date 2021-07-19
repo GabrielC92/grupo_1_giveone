deb0ea94945d97c6a6471c2b5477016fff3d03b7
GabrielC92 <gabriel.calla2012@gmail.com>
2021-07-18 21:56:48 -0300

    estructura de carpetas

---
+++ sprint2/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3030;
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname,'views','index.html')));
+app.get('/producto', (req,res) => res.sendFile(path.join(__dirname,'views','productDetail.html')));
+app.get('/cart', (req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')));
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
+app.get('/registro', (req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+
+app.listen(port, () => console.log(`Server running in http://localhost:${port}`));
