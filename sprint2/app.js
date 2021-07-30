ba729e549e95d2909d5844cdb5e93180d8d68c89
GabrielC92 <gabriel.calla2012@gmail.com>
2021-07-30 00:40:14 -0300

    vistas extra y estilado

--- sprint2/app.js
+++ sprint2/app.js
@@ -11,5 +11,7 @@ app.get('/cart', (req,res) => res.sendFile(path.join(__dirname,'views','productC
 app.get('/formasDePago', (req,res) => res.sendFile(path.join(__dirname,'views','formaDePago.html')));
 app.get('/login', (req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
 app.get('/registro', (req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+app.get('/forgot', (req,res) => res.sendFile(path.join(__dirname,'views','forgot.html')));
+app.get('/forgot2', (req,res) => res.sendFile(path.join(__dirname,'views','forgot2.html')));
 
 app.listen(port, () => console.log(`Server running in http://localhost:${port}`));
