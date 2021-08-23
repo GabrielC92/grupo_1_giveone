0b1c9614356e9ad1eb0e3aa1ffbfb88b6f163792
GabrielC92 <gabriel.calla2012@gmail.com>
2021-08-23 02:18:18 -0300

    instala y configura multer

--- sitio/src/routes/admin.js
+++ sitio/src/routes/admin.js
@@ -1,10 +1,24 @@
 let express = require('express');
 let router = express.Router();
+const path = require('path');
+const multer = require('multer');
 let {create,store,edit,update,destroy} = require('../controllers/adminController');
+
+// Configuración Multer
+const storage = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null,'./public/images/products');
+    },
+    filename: (req,file,cb) => {
+        cb(null,`img-${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const uploadFile = multer({storage});
 
 // Rutas del administrador
 router.get('/create', create);
-router.post('/create', store);
+router.post('/create', uploadFile.single('image'), store);
 
 router.get('/edit/:id', edit);
 router.put('/edit/:id', update);
