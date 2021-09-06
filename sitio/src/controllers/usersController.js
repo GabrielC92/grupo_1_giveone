80e57123868304db9d1d6a78ae13e1b70c0290f0
GabrielC92 <gabriel.calla2012@gmail.com>
2021-09-06 19:18:18 -0300

    crear users validator

--- sitio/src/controllers/usersController.js
+++ sitio/src/controllers/usersController.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const users = require('../data/users.json');
+const {validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
+
 module.exports = {
     login: (req,res) => {
         return res.render('login',{
