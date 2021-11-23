59c8f7ef2491c48699c9556de2be925fdcfed5aa
GabrielC92 <gabriel.calla2012@gmail.com>
2021-11-23 04:06:04 -0300

    correcciones

--- sitio/public/javascript/productDelete.js
+++ sitio/public/javascript/productDelete.js
@@ -1,11 +1,11 @@
-let { fire } = require('sweetalert2');
+import Swal from 'sweetalert2/dist/sweetalert2.js'
 let productDelete = document.getElementById('product-delete');
 
 console.log('productDelete.js success!');
 
 productDelete.addEventListener("submit", e => {
   e.preventDefault();
-  fire({
+  Swal.fire({
       title: '¿Estás seguro de borrar este producto?',
       text: "¡No podrás revertir esta acción!",
       icon: 'warning',
@@ -15,7 +15,7 @@ productDelete.addEventListener("submit", e => {
       confirmButtonText: '¡Delete it!'
     }).then((result) => {
       if (result.isConfirmed) {
-        fire(
+        Swal.fire(
           '¡Eliminado!',
           'El producto fue eliminado de la base de datos.',
           'success'
